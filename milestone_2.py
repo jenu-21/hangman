082e70521f3c551fa5846346e80fd4533efa85fc
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-05-23 14:44:27 +0100

    Add milestone 4 code

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,7 @@
 import random
 
 word_list = ["banana", "apple", "pomegranate", "blueberry", "rambutan"]
-print(word_list)
+print(word_list)    
 
 word = random.choice(word_list)
 print(word)
@@ -13,4 +13,3 @@ if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
-        
